3f6f97da60823e75f0ac621e25e86ff07c13de70
ramphy0x90 <ramphy11@hotmail.com>
2022-06-05 13:45:55 +0200

    Frontend struct

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './containers/home/home.component';
+import { LoginComponent } from './containers/login/login.component';
+import { SignupComponent } from './containers/signup/signup.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import { SignupFormComponent } from './components/signup-form/signup-form.component';
+import { HomeAppComponent } from './containers/home-app/home-app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    SignupComponent,
+    NavBarComponent,
+    LoginFormComponent,
+    SignupFormComponent,
+    HomeAppComponent,
   ],
   imports: [
     BrowserModule,
