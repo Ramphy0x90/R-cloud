514a16196c389849a2eaefa1dc8bc8db267ebccd
ramphy0x90 <ramphy11@hotmail.com>
2022-06-11 01:54:12 +0200

    Items navigation

--- client/src/app/containers/home-app/home-app.component.ts
+++ client/src/app/containers/home-app/home-app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ItemService } from 'src/app/services/item-service.service';
 
 @Component({
   selector: 'app-home-app',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-app.component.css']
 })
 export class HomeAppComponent implements OnInit {
+  path: any;
+  pathHistory: {folder: string, path: string}[] = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private itemService: ItemService) {
+    this.itemService.currentPath.subscribe((currentPath) => {
+      this.path = currentPath;
+    });
   }
 
+  ngOnInit(): void {
+    this.itemService.getContent();
+  }
+
+  getContent(parent: string | any) {
+    let nextPath = this.path[parent].path;
+    this.pathHistory.push({folder: parent, path: nextPath});
+
+    this.itemService.getContent(nextPath);
+  }
+
+  comeBack(folderTo: number) {
+    let folderInfo = this.pathHistory[folderTo];
+    let newPath = folderInfo.path.substring(0, folderInfo.path.length - folderInfo.folder.length)
+
+    this.pathHistory.splice(folderTo);
+    this.itemService.getContent(newPath);
+  }
 }
--- client/src/app/services/item-service.service.ts
+++ client/src/app/services/item-service.service.ts
@@ -1,9 +1,36 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ItemServiceService {
+export class ItemService {
+  baseUri: string = 'http://localhost:3435/api/folder';
+  headers!: HttpHeaders;
+  currentPath: EventEmitter<any> = new EventEmitter();
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    let userToken: any = '';
+    
+    if(undefined !== localStorage.getItem('token') !== null) {
+      userToken = localStorage.getItem('token');
+    }
+
+    this.headers = new HttpHeaders().set('content-type', 'apllication/json')
+                                    .set('x-access-token', userToken);
+  }
+
+  getContent(path: string = '') {
+    let queryParams = new HttpParams();
+    queryParams = queryParams.append("path", path);
+
+    this.http.get(`${this.baseUri}`, {headers: this.headers, params: queryParams}).subscribe({
+      next: (response) => {
+        this.currentPath.emit(response);
+      },
+      error: (error) => {
+        console.log(error);
+      }
+    });
+  }
 }
