a5306bccf8cec81e3d817fca3f2d3cf56e9ed271
ramphy0x90 <ramphy11@hotmail.com>
2022-06-28 23:14:17 +0200

    Items menu

--- client/src/app/components/item-tools/item-tools.component.ts
+++ client/src/app/components/item-tools/item-tools.component.ts
@@ -6,8 +6,8 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
   styleUrls: ['./item-tools.component.css']
 })
 export class ItemToolsComponent implements OnInit {
-  @Output() buttonsEvent = new EventEmitter<{onNew: boolean, onUpload: boolean, onEdit: boolean}>();
-  buttons = {onNew: false, onUpload: false, onEdit: false};
+  @Output() buttonsEvent = new EventEmitter<{onNew: boolean, onUpload: boolean, onEdit: boolean, onDownload: boolean}>();
+  buttons = {onNew: false, onUpload: false, onEdit: false, onDownload: false};
 
   constructor() {
 
@@ -19,8 +19,11 @@ export class ItemToolsComponent implements OnInit {
   setEvent(btn: string) {
     if(btn == 'edit') this.buttons.onEdit = !this.buttons.onEdit;
     if(btn == 'upload') this.buttons.onUpload = true;
+    if(btn == 'download') this.buttons.onDownload = true;
 
     this.buttonsEvent.emit(this.buttons);
+
+    this.buttons.onDownload = false;
   }
 
   setUploadModalEvent(event: any) {
